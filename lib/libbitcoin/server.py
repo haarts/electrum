f5cb6c4b20fd8753bd81da0c6328e1e2aef68f59
Harm Aarts <harmaarts@gmail.com>
2018-06-14 16:05:51 +0200

    Add libbitcoin stubs
    These are the first steps to implement the libbitcoin backends.
    I imagine a lot of changes still.

---
+++ lib/libbitcoin/server.py
@@ -0,0 +1,17 @@
+import asyncio
+import pylibbitcoin.client
+
+class Server:
+    def __init__(self, connection_details):
+        def url_from(server):
+            return "tcp://" + server["dns"] + ":" + str(server["ports"]["public"])
+
+        self.url = url_from(connection_details)
+        self._client = pylibbitcoin.client.Client(self.url)
+        self.last_height = self.last_height()
+
+    async def disconnect(self):
+        return self._client.stop()
+
+    def last_height(self):
+        return asyncio.get_event_loop().run_until_complete(self._client.last_height())
