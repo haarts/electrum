74ca453a63f31caa434e479368962dbe38230213
Harm Aarts <harmaarts@gmail.com>
2018-06-19 15:30:29 +0200

    Implement several public methods
    Methods which are defined in the old network module. Implementing these
    makes sure the transition from ElectrumX to libbitcoin is as painless as
    possible. We might even end up with a drop in replacement!

--- lib/tests/test_libbitcoin.py
+++ lib/tests/test_libbitcoin.py
@@ -1,37 +1,67 @@
 import unittest
 import asyncio
 import asynctest
-import pylibbitcoin
+import pylibbitcoin.client
+import zmq.asyncio
 
+from unittest.mock import MagicMock
 from lib.simple_config import SimpleConfig
 from lib.libbitcoin.libbitcoin import Libbitcoin
 from lib.libbitcoin.server import Server
 
 class TestLibbitcoin(unittest.TestCase):
     def setUp(self):
-        self.libbitcoin = Libbitcoin(SimpleConfig())
+        Libbitcoin._servers_from = lambda x,y: [MagicMock(autospec=Server)]
+        self.libbitcoin = Libbitcoin(None)
+        self.libbitcoin.disconnect = lambda : None
 
     def tearDown(self):
-        self.libbitcoin.disconnect()
+        pass
 
     def test_is_connected(self):
+        self.assertFalse(self.libbitcoin.is_connected())
+
+        self.libbitcoin._servers = [MagicMock(autospec=Server, last_height=1)]
         self.assertTrue(self.libbitcoin.is_connected())
 
+    def test_is_connecting(self):
+        self.assertFalse(self.libbitcoin.is_connecting())
+
     def test_servers_wo_recent_servers(self):
-        self.assertEqual(4, len(self.libbitcoin.get_servers()))
+        self.assertEqual(1, len(self.libbitcoin.get_servers()))
 
     @unittest.skip("pending")
     def test_servers_w_recent_servers(self):
         # TODO manipulate ~/.electrum/recent_servers
         self.assertEqual(2, len(self.libbitcoin.get_servers()))
 
+    def test_set_parameters_new_server(self):
+        pylibbitcoin.client.Client = MagicMock(autospec=Server)
+        self.assertEqual(1, len(self.libbitcoin._servers))
+        self.libbitcoin.set_parameters("example.com", "9091", "", None, True)
+        self.assertEqual("tcp://example.com:9091", self.libbitcoin.active_server.url)
+        self.assertEqual(2, len(self.libbitcoin._servers))
+
+    def test_set_parameters_existing_server(self):
+        pylibbitcoin.client.Client = MagicMock(autospec=Server)
+        self.assertEqual(1, len(self.libbitcoin._servers))
+        self.libbitcoin.set_parameters("example.com", "9091", None, None, True)
+        self.libbitcoin.set_parameters("example.com", "9091", None, None, True)
+        self.assertEqual(2, len(self.libbitcoin._servers))
+
+    def test_switch_to_interface(self):
+        pylibbitcoin.client.Client = MagicMock(autospec=Server)
+        self.libbitcoin.switch_to_interface("example.com:9091")
+        self.assertEqual(2, len(self.libbitcoin._servers))
+
 
 class TestServer(asynctest.TestCase):
     def test_initial_api_call(self):
         height = 500_000
         client_mock = asynctest.mock.MagicMock()
-        client_mock.return_value.last_height = asynctest.CoroutineMock(return_value=height)
+        client_mock.return_value.last_height = \
+            asynctest.CoroutineMock(return_value=(None, height))
         pylibbitcoin.client.Client = client_mock
 
-        server = Server({"dns": "smt", "ports": {"public": 9091}})
+        server = Server({"dns": "smt", "ports": {"public": 9091}}, None)
         self.assertEqual(height, server.last_height)
