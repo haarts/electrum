a5f33b0a09aaa12e0a148755e81c156c9cf78fc8
Harm Aarts <harmaarts@gmail.com>
2018-06-19 15:28:56 +0200

    Pass ClientSettings
    Also move blocking stuff out the constructor. Constructors can't handle await
    stuff.

--- lib/libbitcoin/libbitcoin.py
+++ lib/libbitcoin/libbitcoin.py
@@ -1,28 +1,46 @@
 import os
 import asyncio
 
-from lib import constants 
-from lib import simple_config
-from .protocol import Protocol
-from .server import Server
+from lib import constants
+from lib.triggers import Triggers
+from lib.libbitcoin.protocol import Protocol
+from lib.libbitcoin.server import Server
 
-class Libbitcoin(Protocol):
+class Libbitcoin(Protocol, Triggers):
     """ Libbitcoin brings everything together.
     """
-    def __init__(self, config: simple_config.SimpleConfig):
-        self.config = config
-        self._servers = self._servers_from(self._servers_from_file())
+    def __init__(self, client_settings):
+        self._client_settings = client_settings
+        self._auto_connect = True
+        self._is_connecting = False
+        self._servers = Libbitcoin._servers_from(self._client_settings,
+            Libbitcoin._servers_from_file())
+        self.active_server = None
+
+    def connect(self):
+        """ This call doesn't block. """
+        self._is_connecting = True
+        def no_longer_connecting(future):
+            self._is_connecting = False
+
+        future = asyncio.gather(
+            *[server.connect() for server in self._servers]
+        )
+        future.add_done_callback(no_longer_connecting)
+        return asyncio.get_event_loop().create_task(future)
 
     def disconnect(self):
+        """ This call blocks. """
         loop = asyncio.get_event_loop()
         loop.run_until_complete(asyncio.gather(
             *[server.disconnect() for server in self._servers]
         ))
 
-    def _servers_from(self, servers):
-        return [Server(server) for server in servers]
+    def _servers_from(client_settings, servers):
+        return [Server(connection_details, client_settings)
+            for connection_details in servers]
 
-    def _servers_from_file(self):
+    def _servers_from_file():
         # TODO read recent servers OR
         # read packaged servers
         return constants.read_json(
