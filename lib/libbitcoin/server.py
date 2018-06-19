19d2faeb5224c74a08159bfab1222a3557ffa271
Harm Aarts <harmaarts@gmail.com>
2018-06-19 15:26:03 +0200

    Require passing of Context
    All sockets should share one Context.
    Don't make a blocking call in the constructor.

--- lib/libbitcoin/server.py
+++ lib/libbitcoin/server.py
@@ -2,16 +2,23 @@ import asyncio
 import pylibbitcoin.client
 
 class Server:
-    def __init__(self, connection_details):
+    def __init__(self, connection_details, settings):
         def url_from(server):
             return "tcp://" + server["hostname"] + ":" + str(server["ports"]["public"])
 
         self.url = url_from(connection_details)
-        self._client = pylibbitcoin.client.Client(self.url)
-        self.last_height = self.last_height()
+        self._client = pylibbitcoin.client.Client(self.url, settings=settings)
 
     async def disconnect(self):
-        return self._client.stop()
+        return await self._client.stop()
+
+    async def connect(self):
+        error_code, last_height = await self._client.last_height()
+        self.last_height = last_height
+        return error_code
+
+    def is_connected(self):
+        return self.last_height > 0
 
     def last_height(self):
         return asyncio.get_event_loop().run_until_complete(self._client.last_height())
