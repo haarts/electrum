4f5b7769f4a6b4558a5e319977defe7c3aeec78a
Harm Aarts <harmaarts@gmail.com>
2018-07-13 10:47:17 +0200

    Remove mentions interface in non network modules

--- electrum/network.py
+++ electrum/network.py
@@ -851,14 +851,20 @@ class Network(util.DaemonThread):
                 if self.config.is_fee_estimates_update_required():
                     self._request_fee_estimates()
 
-    def request_chunk(self, interface, index):
+    def fetch_missing_headers_around(self, tx_height):
+        """ This method fetches blocks in batches of 2016 block around a specific
+        height. The number 2016 is the same interval the checkpoints are using and
+        appear to be the maximum an ElectrumX server will return.
+        """
+        index = tx_height // 2016
         if index in self.requested_chunks:
             return
-        interface.print_error("requesting chunk %d" % index)
+
         self.requested_chunks.add(index)
-        height = index * 2016
-        self.queue_request('blockchain.block.headers', [height, 2016],
-                           interface)
+        self._queue_request(
+            'blockchain.block.headers',
+            [index * 2016, 2016],
+            self.interface)
 
     def _on_block_headers(self, interface, response):
         '''Handle receiving a batch of block headers'''
@@ -869,6 +875,7 @@ class Network(util.DaemonThread):
         if result is None or params is None or error is not None:
             interface.print_error(error or 'bad response')
             return
+
         # Ignore unsolicited chunks
         height = params[0]
         index = height // 2016
@@ -878,14 +885,13 @@ class Network(util.DaemonThread):
         else:
             interface.print_error("received chunk %d" % index)
         self.requested_chunks.remove(index)
-        hexdata = result['hex']
-        connect = blockchain.connect_chunk(index, hexdata)
+        connect = blockchain.connect_chunk(index, result['hex'])
         if not connect:
             self._connection_down(interface.server)
             return
         # If not finished, get the next chunk
         if index >= len(blockchain.checkpoints) and blockchain.height() < interface.tip:
-            self.request_chunk(interface, index+1)
+            self.fetch_missing_headers_around(height + 2016)
         else:
             interface.mode = 'default'
             interface.print_error('catch up done', blockchain.height())
@@ -1012,7 +1018,7 @@ class Network(util.DaemonThread):
         # If not finished, get the next header
         if next_height is not None:
             if interface.mode == 'catch_up' and interface.tip > next_height + 50:
-                self.request_chunk(interface, next_height // 2016)
+                self.fetch_missing_headers_around(next_height)
             else:
                 self.request_header(interface, next_height)
         else:
